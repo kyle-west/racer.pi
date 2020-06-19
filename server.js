790e6aff18928673a01c89519d2f60069f8fccb1
Kyle West <kylewestrrr@gmail.com>
2020-06-19 17:30:51 -0600

    Revert "[CONFIG] use .env to share config between python and JS"
    This reverts commit 573c4ef732ee28c0b4bd8e34c1d2e472a8ec0ed1.

--- server.js
+++ server.js
@@ -1,69 +1,30 @@
-require('dotenv').config()
 const express = require('express')
 const path = require('path')
 const ip = require("ip")
 const app = express()
 const WebSocket = require('ws')
+const watch = require('node-watch')
 const { interpolate } = require('./lib/util')
-const Race = require('./lib/Race')
 
-const {
-  BTN_START,
-  BTN_LANE_1,
-  BTN_LANE_2,
-  BTN_LANE_3,
-  BTN_LANE_4,
-  SERVER_PORT,
-  WS_PORT,
-} = process.env
+const port = process.env.PORT || 8000
+const wsPort = port + 1
 
-// --------------------------------------------------------------------------
-// Race data management
-// --------------------------------------------------------------------------
-const RACE = new Race({debug: true})
-function raceEntry ({state, name}) {
-  // only listen to ButtonDown events
-  if (state === 1) {
-    switch(name) {
-      case BTN_START:  !RACE.active ? RACE.start() : RACE.end(); break;
-      case BTN_LANE_1: RACE.crossFinishLine(BTN_LANE_1); break;
-      case BTN_LANE_2: RACE.crossFinishLine(BTN_LANE_2); break;
-      case BTN_LANE_3: RACE.crossFinishLine(BTN_LANE_3); break;
-      case BTN_LANE_4: RACE.crossFinishLine(BTN_LANE_4); break;
-    }
-  }
-}
+const wss = new WebSocket.Server({ port: wsPort })
 
-// --------------------------------------------------------------------------
-// Server / WebSocket Config 
-// --------------------------------------------------------------------------
-const wss = new WebSocket.Server({ port: WS_PORT })
-
-
-
-// --------------------------------------------------------------------------
-// Service handlers
-// --------------------------------------------------------------------------
 app.use(express.static('public'))
 
-app.get('/config', (req, res) => res.json({ SERVER_PORT, WS_PORT }))
+app.get('/config', (req, res) => res.json({ port, wsPort }))
 
 app.get('/gpio', (req, res) => {
-  const entry = {
-    name : interpolate(req.query.name),
-    state:  interpolate(req.query.state),
-  }
+  const entry = {}
+  entry.name = interpolate(req.query.name)
+  entry.state = interpolate(req.query.state)
   console.log(entry)
-  raceEntry(entry)
   res.sendStatus(204)
 })
 
 app.get('/', (req, res) => res.sendFile(path.resolve('.', './public/index.html')))
 
-
-// --------------------------------------------------------------------------
-// WebSocket handlers
-// --------------------------------------------------------------------------
 // Broadcasts data to every connection
 // function broadcast(type, data = {}) {
 //   wss.clients.forEach(function each(client) {
@@ -72,10 +33,11 @@ app.get('/', (req, res) => res.sendFile(path.resolve('.', './public/index.html')
 //     }
 //   });
 // };
+
 wss.on('connection', ws => ws.send('hello from server'))
 
-app.listen(SERVER_PORT, () => {
-  let localhostURL = SERVER_PORT === 80 ? 'http://localhost/' : `http://localhost:${SERVER_PORT}/`
-  let ipURL = SERVER_PORT === 80 ? `http://${ip.address()}/` : `http://${ip.address()}:${SERVER_PORT}/`
+app.listen(port, () => {
+  let localhostURL = port === 80 ? 'http://localhost/' : `http://localhost:${port}/`
+  let ipURL = port === 80 ? `http://${ip.address()}/` : `http://${ip.address()}:${port}/`
   console.log(`${localhostURL}\n  ${ipURL}`)
 })
