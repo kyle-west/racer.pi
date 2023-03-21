e429b2bc43bb3004065d08a91823338062c1d317
Kyle West <kylewestrrr@gmail.com>
2023-03-21 07:09:40 -0600

    [lane assignments] improve display slightly

--- public/views/Round1.js
+++ public/views/Round1.js
@@ -1,10 +1,37 @@
 import env from '/env'
-import { WebComponent, wc, dom, register, inline } from '../dom.js'
+import { WebComponent, wc, dom, register, inline, css } from '../dom.js'
 import CarConfig from './CarConfig.js'
 import { localStorage } from '../storage.js'
 
 const totalLanes = env.LANE_COUNT
 const halfOfTheLanes = Math.floor(totalLanes/2)
+
+const styles = css`
+  table {
+    border-collapse: collapse;
+    width: 100%;
+  }
+
+  table td, table th {
+    border: 1px solid #ddd;
+    padding: 4px;
+  }
+
+  table tr:nth-child(even) { background-color: #f2f2f2; }
+  table tr:hover { background-color: #ddd; }
+
+  table thead th {
+    padding-top: 6px;
+    padding-bottom: 6px;
+    text-align: left;
+    background-color: #333;
+    color: white;
+  }
+
+  table tbody th {
+    text-align: start;
+  }
+`
 
 const messageTemplate = wc`
   <h1>First Elimination Round</h1>
@@ -22,16 +49,8 @@ const messageTemplate = wc`
   <button name="configure-racers">Configure Racers</button>
 `
 
-const heatResults = ({ continuing = [], eliminated = [] }) => dom`
-  ${inline(continuing.length > 0 && '<h3>Winners!</h3><ol>')}
-  ${inline(continuing.map(({ name, weight, time }) => `<li>${name}[${weight}] - ${time}</li>`))}
-  ${inline(continuing.length > 0 && '</ol>')}
-  ${inline(eliminated.length > 0 && `<h3>Eliminated</h3><ol start="${halfOfTheLanes}">`)}
-  ${inline(eliminated.map(({ name, weight, time }) => `<li>${name}[${weight}] - ${time}</li>`))}
-  ${inline(eliminated.length > 0 && '</ol>')}
-`
-
 const round = wc`
+  ${styles}
   <h1>First Elimination Round</h1>
   <pre id="timer"></pre>
   <div id="heats"></div>
@@ -74,13 +93,23 @@ export default class Round1 extends WebComponent {
     this.$('#heats').appendChild(dom`
       <section id="heat-${this.heatNumber}">
         <h2>Heat ${this.heatNumber}</h2>
-
-        <p>Lane Assignments:</p>
-        <dl>
+        <table>
+          <thead>
+            <tr><th>Assignment</th><th>Car Name</th><th>Weight</th><th>Time</th><th>Place</th><th>Status</th></tr>
+          </thead>
+          <tbody>
           ${inline(Object.entries(assignments).map(([laneNumber, { name, weight }]) => `
-            <dt>Lane ${laneNumber}</dt><dd>${name}[${weight}]</dd>
+            <tr id="lane-${laneNumber}">
+              <td>Lane ${laneNumber}</td>
+              <th>${name}</th>
+              <td>${weight} oz</td>
+              <td id="lane-${laneNumber}-time"></td>
+              <td id="lane-${laneNumber}-place"></td>
+              <td id="lane-${laneNumber}-status"></td>
+            </tr>
           `))}
-        </dl>
+          </tbody>
+        </table>
 
         <button id="action" name="start-race">Start!</button>
         <div id="${this.heatNumber}-results"></div>
