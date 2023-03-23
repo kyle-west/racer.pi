ef3277add5f12c935c55d89dcb3e9d0c8f39d79a
Kyle West <kyle@familysearch.org>
2023-03-22 20:43:41 -0600

    [round1] round one elimination working

--- public/views/Round1.js
+++ public/views/Round1.js
@@ -33,6 +33,22 @@ const styles = css`
   table tbody th {
     text-align: start;
   }
+
+  tr.Continuing { background-color: lightblue !important; }
+  tr.Eliminated { background-color: lightpink !important; }
+  tr.Finalist   { background-color: lightgreen !important; }
+
+  .hidden {
+    display: none;
+  }
+
+  td {
+    transition: all 0.3s;
+  }
+
+  #timer {
+    font-size: 30px;
+  }
 `
 
 const messageTemplate = wc`
@@ -54,7 +70,7 @@ const messageTemplate = wc`
 const round = wc`
   ${styles}
   <h1>First Elimination Round</h1>
-  <pre id="timer"></pre>
+  <pre id="timer">Ready</pre>
   <div id="heats"></div>
 `
 
@@ -100,16 +116,17 @@ export default class Round1 extends WebComponent {
     const ordered = [...continuing].sort(({times: a}, {times:b}) => a.length - b.length)
     const racers = ordered
       .slice(0, totalLanes)
-      .sort(() => Math.random()*2 - Math.random()*2)
+      .sort(() => Math.random() - Math.random())
     return Object.fromEntries(racers.map((racer, index) => [index+1, racer]))
   }
 
-  newHeat() {
+  newHeat(finalHeat) {
+    this.finalHeat = finalHeat
     this.heatNumber = (this.heatNumber || 0) + 1;
     const assignments = this.getHeatMemberLaneAssignments()
     this.laneAssignments = assignments
 
-    this.$('#heats').appendChild(dom`
+    this.$('#heats').prepend(dom`
       <section id="heat-${this.heatNumber}">
         <h2>Heat ${this.heatNumber}</h2>
         <table>
@@ -128,33 +145,95 @@ export default class Round1 extends WebComponent {
           `))}
           </tbody>
         </table>
+
+        <div class="action hidden">
+          <button name="restart">Redo Race</button>
+          <button name="accept">Accept & Continue</button>
+        </div
       </section>
     `)
   }
 
   onRaceStart () {
+    if (this._unsubmitted) {
+      this.onClickRestart()
+    }
+
     const timerDOM = this.$('#timer')
     this.timer = this.timer || new Timer((time) => {
-      timerDOM.innerHTML = `${time}ms`
-    })
+      timerDOM.innerHTML = `Time: ${time/1000}s`
+    }, 17)
     this.timer.start()
   }
 
   onRaceUpdate ({ detail: { lanes = {} } }) {
     this.laneData = lanes
-    Object.entries(lanes).sort(([_,a],[__,b]) => a-b).forEach(([laneNumber, time], idx) => {
+    const laneEntries = Object.entries(lanes).sort(([_,a],[__,b]) => a-b)
+
+    if (laneEntries.length === 1) {
+      this.timer?.stop()
+      const [lane, time] = laneEntries[0]
+      this.$('#timer').innerHTML = `Time: ${time}s ${this.laneAssignments[lane].name} Wins!`
+    }
+    
+    laneEntries.forEach(([laneNumber, time], idx) => {
+      if (!this.$(`#heat-${this.heatNumber}-lane-${laneNumber}`)) return // for partially full track
+
       this.$(`#heat-${this.heatNumber}-lane-${laneNumber}-time`).innerHTML = time+ 's'
       
       const order = formatOrdinals(idx + 1)
       this.$(`#heat-${this.heatNumber}-lane-${laneNumber}-place`).innerHTML = order
 
-      const status = idx < halfOfTheLanes ? 'Continuing' : 'Eliminated'
+      const win = this.finalHeat ? 'Finalist' : 'Continuing'
+      const status = idx < halfOfTheLanes ? win : 'Eliminated'
       this.$(`#heat-${this.heatNumber}-lane-${laneNumber}-status`).innerHTML = status
+      this.$(`#heat-${this.heatNumber}-lane-${laneNumber}`).classList.add(status)
     })
+
+    if (laneEntries.length === this.continuing.length) {
+      this.onRaceEnd()
+    }
   }
 
-  onRaceEnd (data) {
-    this.timer?.stop()
+  onRaceEnd () {
+    this.$('.action').classList.remove('hidden')
+    this._unsubmitted = true
+  }
+
+  onClickRestart () {
+    this.$('.action').classList.add('hidden')
+    this.$$(`[id="heat-${this.heatNumber}"] td[id]`).forEach(elem => elem.innerHTML = '')
+    this.$$(`[id="heat-${this.heatNumber}"] .Continuing`).forEach(elem => elem.classList.remove('Continuing'))
+    this.$$(`[id="heat-${this.heatNumber}"] .Eliminated`).forEach(elem => elem.classList.remove('Eliminated'))
+    this.$$(`[id="heat-${this.heatNumber}"] .Finalist`).forEach(elem => elem.classList.remove('Finalist'))
+    this.$('#timer').innerHTML = 'Ready.'
+    this.laneData = null
+    this._unsubmitted = false
+  }
+
+  onClickAccept () {
+    this.$('#timer').innerHTML = 'Ready.'
+    this.$('.action').remove()
+
+    const racers = Object.entries(this.laneData)
+      .sort(([_,a],[__,b]) => a-b)
+      .map(([lane, time]) => {
+        this.laneAssignments[lane].times.push(time)
+        return this.laneAssignments[lane]
+      })
+
+    const loserIds = racers.slice(halfOfTheLanes, totalLanes).map(({id}) => id)
+    this.eliminated = this.continuing.filter(({id}) => loserIds.includes(id))
+    this.continuing = this.continuing.filter(({id}) => !loserIds.includes(id))
+
+    if (this.finalHeat) {
+      localStorage.set('finalists', [...localStorage.get('finalists', []), ...this.continuing])
+      // todo: update CarGroup.is storage to include times
+      debugger
+      return
+    }
+
+    this.newHeat(this.continuing.length <= totalLanes)
   }
 }
 
