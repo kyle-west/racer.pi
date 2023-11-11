9c142fe17de02d1fdebb5eb4869d519beaa32ace
Kyle West <kylewestrrr@gmail.com>
2023-11-11 11:27:07 -0700

    [standalone] get the stand alone script working

--- standalone.sh
+++ standalone.sh
@@ -1,1 +1,24 @@
-chromium-browser http://localhost:5000
+source .env # we need this for the SERVER_PORT envar
+
+wd="$(pwd)"
+
+# this script is ran in a clean context where PATH is not set,
+# so we have to specify the runtimes.
+_node='/usr/bin/node'
+_npm='/usr/bin/npm'
+_python='/usr/bin/python'
+updatePathCommand='export PATH="/usr/bin/:$PATH'
+
+if [ -d /home/sushi/.local/share/fnm ]; then
+  eval `/home/sushi/.local/share/fnm/fnm env`
+  _node="$(/usr/bin/which node)"
+  _npm="$(/usr/bin/which npm)"
+  updatePathCommand='eval `/home/sushi/.local/share/fnm/fnm env`; export PATH="/usr/bin/:/home/sushi/.local/share/fnm:$PATH"'
+fi
+
+# Run the node server, the python script
+lxterminal -e "($_node $wd/server.js)" & disown
+lxterminal -e "$updatePathCommand; $wd/node_modules/.bin/dotenv $_python $wd/main.py; sleep 5s" & disown
+
+# open the browser in a new tab pointed towards our app
+chromium-browser "http://localhost:$SERVER_PORT"
