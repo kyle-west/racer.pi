a3ad6488e6b3979f38763d9bb7523a1b47ae1529
Ralph West <ralphrwest@msn.com>
2023-11-11 17:01:29 +0000

    Runnable with hw

--- main.py
+++ main.py
@@ -24,6 +24,17 @@ def initialize():
 
     print ('startpint ' +str(GPIO.input(startpin)))     
 
+def beginracecallback(channel):
+    if GPIO.input(startpin):
+        print('rising signal recieved, starting timer!!!!')
+        run_race(server_port)
+        print('Race completed!')
+        print('Waiting for the start race button to be pressed.')
+    else:
+        print('falling signal detected, waiting for button release')
+
+
+
 def teardown():
     GPIO.cleanup()
 
@@ -39,6 +50,9 @@ def start_race(server_port):
 def finish_lane(server_port, lane_number, lane_time):
     """Calls the POST <server-port>/gpio/time?lane=<lane-number>&time=<lane-time> API endpoint."""
 
+    if (lane_time == 0):
+        lane_time = maxtime
+
     url = f"http://localhost:{server_port}/gpio/time"
 
     params = {
@@ -47,7 +61,7 @@ def finish_lane(server_port, lane_number, lane_time):
     }
 
     response = requests.post(url, params=params)
-    print(response)
+    print("Lane " + str(lane_number) + " with time = " + str(lane_time) + " has been send to the racer application.")
     return response
 
 def calculatetime(start):
@@ -55,66 +69,50 @@ def calculatetime(start):
     return  end - start
    
 def run_race(server_port):
-    print('waiting for hardware to send the start signal.')
-    while not GPIO.input(startpin):
-        pass
-    print('Recieved signal and starting timer!!!!!!')    
-    
-     #Set each line count to 0
+    start_race(server_port);
+    #Set each line count to 0
     lane1time = 0
     lane2time = 0
     lane3time = 0
     lane4time = 0
     lane5time = 0  # print('3...')
-    time.sleep(.001)
+    #time.sleep(.001)
     lane6time = 0
     currenttime = 0
-        
-    start_race(server_port);
+            
     #Now read the start time for all lanes
     start = time.time()
 
-    #Loop for 10 seconds and read input for each lane to end race
-    while time.time()- start < 10:
+    #Loop for 5 seconds and read input for each lane to end race
+    while time.time()- start < maxtime:
         #print(start)
         if GPIO.input(lane1) and not lane1time:
              lane1time =  calculatetime(start)
-             print('lane 1 =' + str(lane1time))
         if GPIO.input(lane2) and not lane2time:
              lane2time =  calculatetime(start)
-             print('lane 2 =' + str(lane2time))
         if GPIO.input(lane3) and not lane3time:
              lane3time =  calculatetime(start)
-             print('lane 3 =' + str(lane3time))
         if GPIO.input(lane4) and not lane4time:
              lane4time =  calculatetime(start)
-             print('lane 4 =' + str(lane4time))
         if GPIO.input(lane5) and not lane5time:
              lane5time =  calculatetime(start)
-             print('lane 5 =' + str(lane5time))
         if GPIO.input(lane6) and not lane6time:
              lane6time =  calculatetime(start)
-             print('lane 6 =' + str(lane6time))
         if lane1time and lane2time and lane3time and lane4time and lane5time and lane6time:
              break
        
-    print('\n\n')
-    print('lane 1 =' + str(lane1time))
+       
     finish_lane(server_port, 1, lane1time)
-    print('lane 2 =' + str(lane2time))
     finish_lane(server_port, 2, lane2time)
-    print('lane 3 =' + str(lane3time))
     finish_lane(server_port, 3, lane3time)
-    print('lane 4 =' + str(lane4time))
     finish_lane(server_port, 4, lane4time)
-    print('lane 5 =' + str(lane5time))
     finish_lane(server_port, 5, lane5time)
-    print('lane 6 =' + str(lane6time))
     finish_lane(server_port, 6, lane6time)
       
 
 if __name__ == "__main__":
     server_port = 5000
+    maxtime = 30
     #Map each pin number to GPIO pins
     startpin = 5
     lane1 = 6
@@ -126,10 +124,14 @@ if __name__ == "__main__":
 
    
     initialize();
+    print(GPIO.input(startpin))
     print('Running the race track listener service.')
     print('This is a product developed and exclusively owned by Three Generation of Wests. Contact them for licensing agreements.')
-   
+    print('Waiting for the start race button to be pressed.')
+    GPIO.add_event_detect(startpin,GPIO.BOTH,callback=beginracecallback)
+          
     while (True):
-        run_race(server_port)
-
+        pass
+       
+   
     teardown(); 
